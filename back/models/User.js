4a10e4a459383231566fce26c89e1e823d4cd4cf
Mathieu Franchi <math13300@live.fr>
2022-07-02 00:08:13 +0200

    Back modif package etc

--- back/models/User.js
+++ back/models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, match:"/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i"},
+    password: { type: String, required: true, match: "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" },
     
 });
 
