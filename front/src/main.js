2a72cdb02114c2e073e10824b8e501b1db0096b6
Mathieu Franchi <math13300@live.fr>
2022-07-11 16:10:38 +0200

    test commit stp

--- front/src/main.js
+++ front/src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//fontAwesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { far } from '@fortawesome/free-regular-svg-icons' //all regular svg icons
+import { fas } from '@fortawesome/free-solid-svg-icons' //all solid svg icons
+import { fab } from '@fortawesome/free-brands-svg-icons' //all brand svg icons
 
-createApp(App).use(store).use(router).mount('#app')
+/* add icons to the library */
+library.add(fas, far, fab);
+//<FontAwesome :icon="{prefix:'fas',iconName: 'exempleIcon'}"/>  
+createApp(App).use(store).use(router).component('FontAwesome', FontAwesomeIcon).mount('#app')
