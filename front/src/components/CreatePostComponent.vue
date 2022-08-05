6d526de5cc99551d7df11537c51cab0a303df25a
Mathieu Franchi <math13300@live.fr>
2022-08-05 03:12:33 +0200

    button close modal changed + tailwind htmltest

--- front/src/components/CreatePostComponent.vue
+++ front/src/components/CreatePostComponent.vue
@@ -1,31 +1,49 @@
 <template>
+  <!-- MODAL CONTAINER -->
   <div class="modal-container">
-
+    <!-- OVERLAY -->
     <div class="overlay modal-trigger"></div>
-
+    <!-- MODAL -->
     <div class="modal" role="dialog" aria-labelledby="modalTitle" aria-describedby="dialogDesc">
       <div class="modal__header">
-        <button aria-label="close modal" class="close-modal modal-trigger">X</button>
+        <button aria-label="close modal" class="close-modal modal-trigger">
+          <FontAwesome style="color: white;" icon="fa-solid fa-xmark" />
+        </button>
         <h3 class="modal__title fontsize__titles">Créer une publication</h3>
       </div>
       <div class="post__profil">
-        <div class="post__profil__img__container">
-          <img class="post__profil__img" src="../../../back/images/tabasco.png" alt="Photo de profil" />
+        <div v-if="userInfos.photo != null" class="post__profil__img__container">
+          <img class="post__profil__img" :src="userInfos.photo" alt="Photo de profil" />
         </div>
         <div class="post__name__date">
           <h2 class="post__name fontsize__p">
             {{ userInfos.name + ' ' + userInfos.lastname }}
           </h2>
-          <p class="post__date">{{date}}</p>
+          <p class="post__date">{{ date }}</p>
         </div>
       </div>
       <textarea class="description" type="text" placeholder="Quoi de neuf ?"></textarea>
+      <!-- tailwindtest -->
+      <!-- <div class="flex justify-center items-center w-full">
+        <label for="dropzone-file"
+          class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+          <div class="flex flex-col justify-center items-center pt-5 pb-6">
+            <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor"
+              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span>
+              or drag and drop</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+          </div>
+          <input id="dropzone-file" type="file" class="hidden">
+        </label>
+      </div> -->
     </div>
-
+    <!-- MODAL -->
   </div>
-
-
-
+  <!-- MODAL CONTAINER -->
 </template>
 
 <script>
@@ -150,7 +168,7 @@ export default {
   justify-content: center;
   align-items: center;
   position: relative;
-  border-bottom: solid 1px $primary-color;
+  box-shadow: $primary-color 0px 0px 1px;
    .modal__title{
     font-size: 25px;
     -webkit-font-smoothing: antialiased;
@@ -168,8 +186,11 @@ export default {
        cursor: pointer;
        background: #ff365e;
        color: #fff;
+       transition: transform 0.2s ease-in-out;
      }
-     
+     .close-modal:active{
+      transform: scale(0.92);
+     }
 }
 //profil img + name lastname
 .post__profil {
