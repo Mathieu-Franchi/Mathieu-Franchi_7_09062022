74f3a10bf764d6bb85f356265a84f81042f77aaf
Mathieu Franchi <89087885+Mathieu-Franchi@users.noreply.github.com>
2024-06-23 00:20:12 +0200

    Update vue.config.js

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  outputDir: 'dist', // Assurez-vous que cela correspond au répertoire de publication configuré sur Render
+  publicPath: '/',
+  devServer: {
+    proxy: {
+      '/api': {
+        target: process.env.VUE_APP_API_URL || 'http://localhost:3000', // Utilise l'URL de l'API du backend
+        changeOrigin: true,
+      },
+    },
+  },
+});
